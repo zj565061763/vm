f6c0b55a32eb042f4f6293d589f7679ac9d444ed
zjun <565061763@qq.com>
2024-03-21 10:36:16 +0800

    comment

--- lib/src/main/java/com/sd/lib/vm/ViewModel.kt
+++ lib/src/main/java/com/sd/lib/vm/ViewModel.kt
@@ -16,7 +16,8 @@ interface IgnoreActiveIntent
 
 /**
  * 有激活状态的[ViewModel]，默认激活状态为true，可以通过[setActive]来改变激活状态。
- * 当激活状态为false的时候，不处理意图[I]，除非意图是[IgnoreActiveIntent]类型的。
+ * 默认情况下，当激活状态为false的时候，不处理意图[I]，除非意图是[IgnoreActiveIntent]类型的，
+ * 可以通过重写[canDispatchIntent]来改变这种行为
  */
 open class FViewModel<I> : ViewModel() {
 
